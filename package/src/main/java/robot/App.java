be985e8045b6327b7129f388e32773a810d641d4
jeanronald22 <jeroboumg@gmail.com>
2024-02-08 16:02:31 +0100

    ajout de la classe Robot et des tests y associes

--- package/src/main/java/robot/App.java
+++ package/src/main/java/robot/App.java
@@ -1,13 +1,19 @@
 package robot;
 
 /**
- * Hello world!
+ * ici c'est la fonction/classe principale de notre projet
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "je teste encore les branche!" );
+        // declaration et extenciation d'un nouveau robot
+        Robot robot1=new Robot("D2R2");
+        Robot robot2=new Robot("Data");
+
+        robot1.tostring();
+        System.out.println(robot1.isDead());
+        robot1.fire(robot2);
     }
 }
