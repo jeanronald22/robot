a3013cb7242c313a6d70e70e68b0c4835d9939ba
jeanronald22 <jeroboumg@gmail.com>
2024-02-08 22:53:08 +0100

    modification mineur

--- package/src/main/java/robot/App.java
+++ package/src/main/java/robot/App.java
@@ -11,16 +11,16 @@ public class App
         // declaration et extenciation d'un nouveau robot
 
         Robot robot1=new Robot("D2R2");
-        // Robot robot2=new Robot("Data");
-        // robot1.tostring();
-        // System.out.println(robot1.isDead());
-        // robot1.fire(robot2); 
-        // Arena arena1=new Arena(robot1, robot2);
-        // System.out.println("le vainceur est le robot "+" "+arena1.vainceur.getName());
+        Robot robot2=new Robot("Data");
+        robot1.tostring();
+        System.out.println(robot1.isDead());
+        robot1.fire(robot2); 
+        Arena arena1=new Arena(robot1, robot2);
+        System.out.println("le vainceur est le robot "+" "+arena1.vanRobot.getName());
         Fihter f1=new Fihter("salo");
-        // Fihter f2=new Fihter("idiot");
-        // Arena arene2=new Arena(f1, f2);
-        // System.out.println(arene2.vaFihter.getName());
+        Fihter f2=new Fihter("idiot");
+        Arena arene2=new Arena(f1, f2);
+        System.out.println(arene2.vaFihter.getName());
         Arena arene3=new Arena(f1, robot1);
         System.out.println(arene3.vain.getName());
     }
